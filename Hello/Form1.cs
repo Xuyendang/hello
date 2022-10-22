b081ee429fa81a1b33f55a9c3ed92ba6814dd192
Xuyendang <xuyendang2k2@gmail.com>
2022-10-22 13:46:55 +0700

    Sua

--- Hello/Form1.cs
+++ Hello/Form1.cs
@@ -24,10 +24,14 @@ namespace Hello
 
         private void btCong_Click(object sender, EventArgs e)
         {
-            double a = double.Parse(lbA.Text);
-            double b = double.Parse(lbB.Text);
-            double c = a + b;
-            lbC.Text = c.ToString();
+            if (txtA.Text != String.Empty && txtB.Text != String.Empty)
+            {
+                double a = double.Parse(lbA.Text);
+                double b = double.Parse(lbB.Text);
+                double c = a + b;
+                lbC.Text = c.ToString();
+            }
+                
 
         }
     }
